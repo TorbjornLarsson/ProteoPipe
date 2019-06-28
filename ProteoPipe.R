f56e73ee4a1bcf14950130fc1448598a314f705b
TorbjornLarsson <torbjorn.b.g.larsson@gmail.com>
2019-06-28 15:13:56 +0200

    Added report buttons; split screen in several containers.

--- ProteoPipe.R
+++ ProteoPipe.R
@@ -7,8 +7,9 @@
 
   require(stringi)
 
-  # Load widget graphics
+  # Load widget graphics; assumes librarys loaded to the used R
   require(gWidgets2)
+  # To get the toolkit RGTk2, load library gWidgets2RGtk2 into R and accept installing GTK+
   options("guiToolkit"="RGtk2")
   require(tcltk2)
   
@@ -30,16 +31,23 @@
   # GUI Widget
   win <- gwindow("ProteoPipe")
   gr <- ggroup(container = win, horizontal = FALSE)
-  
+
+  # Split screen
+  topgr <- ggroup(container = gr, horizontal = FALSE)
+  bottomgr <- ggroup(container = gr)
+  leftbgr <- ggroup(container = bottomgr, horizontal = FALSE)
+  rightbgr <- ggroup(container = bottomgr, horizontal = FALSE) 
+
   # Select folder textbox and button
-  lbl_dname<- glabel("Folder to run Quality Control on: ", container = gr)
-  txt_dname <- gedit(dname, container = gr)
+  lbl_dname<- glabel("Selected txt folder to run Quality Control on: ", container = topgr)
+  txt_dname <- gedit(dname, container = topgr)
+  
   h1 <- function(...){
     getwd()
     dname <<- tclvalue(tkchooseDirectory())
     svalue(txt_dname) <- dname
   }
-  b1 <- gbutton("Select folder", container = gr, handler = h1)
+  b1 <- gbutton("Select folder", container = leftbgr, handler = h1)
   
   # Run button
   h2 <- function(...){
@@ -47,24 +55,45 @@
     cat("... please wait...\n\n")
     if (dname != ""){
       y <- list.files(path = dname, pattern = "([0-9A-Za-z]+)[.][y][a][m][l]")
-      p <- file.path(normalizePath(dname,"/"), y[[1]]) 
-      print(p)
-      yaml_list_object <- yaml.load_file(p)
+      yp <<- file.path(normalizePath(dname,"/"), y[[1]]) 
+      cat("Generating file", yp, "\n")
+      yaml_list_object <- yaml.load_file(yp)
       r <- createReport(svalue(txt_dname), yaml_list_object)
       }
-    cat("... done!.\n\n")
+    cat("... done!\n\n")
     }
-  b2 <- gbutton("Start run", container = gr, handler = h2)
+  b2 <- gbutton("Run QC", container = leftbgr, handler = h2)
+  
+  # html report button
+  h3 <- function(...){
+    report <- unlist(strsplit(basename(yp), '.yaml'))[1]
+    html_file <- file.path(dirname(yp), paste(report, "_combined.html", sep = ""))
+    browseURL(html_file)
+  }
+  b3 <- gbutton("html", container = leftbgr, handler = h3)
+  
+  # pdf report button
+  h4 <- function(...){
+    report <- unlist(strsplit(basename(yp), '.yaml'))[1]
+    pdf_file <- file.path(dirname(yp), paste(report, "_combined.pdf", sep = ""))
+    browseURL(pdf_file)
+  }
+  b4 <- gbutton("pdf", container = leftbgr, handler = h4)
+  
+  # # Result image
+  # g1 <- gimage(filename = "heatmap.png", dirname = "C:/Users/torla438/Work Folders/Documents/QC/ProteoPipe", container = gr)
+  # size(g1) <- c(960,480)
   
   # Quit button
-  h3 <- function(...){
+  h5 <- function(...){
     dispose(win)
     q()
   }
-  b3 <- gbutton("Quit", container = gr, handler = h3)
+  b4 <- gbutton("Quit", container = rightbgr, handler = h5)
   
-  # Quick fix to keep running in background on target machine
-  # Comment away during other testing
-  #Sys.sleep(1000000)
+  # To keep running on target machine for ~ 10 days if not quitting.
+  # Will listen on GUI @ ~ 0.5 Hz.
+  # Does not seem to accept values much above 10^6 [s]. 
+  Sys.sleep(1000000)
 
 #} # end function
